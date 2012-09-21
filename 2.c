bd4821fa395f3ba962bd833cc452b5c440b68c35
JuThundercats <julianamcguama@yahoo.com.br>
2012-09-20 22:30:04 -0300

    Update 2.c
    Matheus, fiz algumas correções no seu projeto.
    \o

--- 2.c
+++ 2.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-int main(){
+
+int main()
+{
     char a[100];
+    
     printf("digite seu nome: ");
-    scanf("%s",a);
-    printf("%s",&a);
+    scanf("%s",&a);
+    printf("%s",a);
+ 
     return(0);
-    }
+}
